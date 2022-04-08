1e0baade04c7ea11867bb78dec1ba0b8767cf39f
SALEH ALAOUDI <79456369+salaoudi@users.noreply.github.com>
2022-04-08 03:03:00 -0700

    Initial Commit

---
+++ Kaynews/KaynewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaynewsApp.swift
+//  Kaynews
+//
+//  Created by Zayn on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct KaynewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
